46ff939a674c5ad491e989ea0049805a66323c3e
Denisalik <schegletovdenis@mail.ru>
2023-06-09 14:36:02 +0300

    Setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+setup(
+    name='TabNamesCat',
+    version='0.1.0',
+    description='CategorizationTabNames',
+    url='https://github.com/Denisalik/TabNamesCat',
+    author='Denisalik',
+    author_email='schgletovdenis@mail.ru',
+    # license='BSD 2-clause',
+    packages=['model'],
+    install_requires=['scikit-learn>=1.2.2'],
+
+    classifiers=[
+        'Development Status :: Publishing',
+        'Intended Audience :: Science/Research',
+        # 'License :: OSI Approved :: BSD License',
+        # 'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
